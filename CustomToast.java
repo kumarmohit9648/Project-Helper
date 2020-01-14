09dfe926d66e33990fea42cefc4f211ad6956250
kumarmohit9648 <36546754+kumarmohit9648@users.noreply.github.com>
2020-01-15 02:16:15 +0530

    Add files via upload

---
+++ CustomToast.java
@@ -0,0 +1,28 @@
+import android.app.Activity;
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CustomToast {
+
+    public static void initToast(Context context,String msg){
+        Activity act = (Activity) context;
+        LayoutInflater inflater = act.getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast,
+                (ViewGroup)act. findViewById(R.id.custom_lay));
+
+        TextView text = (TextView) layout.findViewById(R.id.toast_text);
+        text.setText(msg);
+        
+        Toast toast = new Toast(context);
+        toast.setGravity(Gravity.BOTTOM|Gravity.CENTER, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+
+        toast.setView(layout);
+        toast.show();
+    }
+}
